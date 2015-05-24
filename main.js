e9a9f7c4f05c910d863e32ff95c859478b6e19cb
Thilo Maurer <tm@thilomaurer.de>
2015-05-24 02:51:00 +0200

    fixes

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ var main = function() {
 		"Maggi.js",
 		"Maggi.UI.js",
 		"Maggi.UI.css",
+		"Maggi.UI.input.css",
+		"Maggi.UI.select.css",
 		"demos/pwcalc.js",
 		"demos/pwcalc.css",
 		"demos/pwcalc.html",
 		"demos/utils.js"],
 		function(project) {
-			project.view.panes[0].fileid=4;
-			project.view.panes[1].fileid=4;
+			project.view.panes[0].fileid=6;
+			project.view.panes[1].fileid=7;
+			project.view.panes[2].fileid=6;
 			d.projects.add(0,project);
 			d.project=project;
 		}
