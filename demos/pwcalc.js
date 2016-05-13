d5011d6b6fd8c48656312a36a97900191ddcb55d
Thilo Maurer <tm@thilomaurer.de>
2016-05-13 18:21:04 +0200

    upgrade pwcalc demo to mui2

--- demos/pwcalc.js
+++ demos/pwcalc.js
@@ -4,31 +4,30 @@ var pwcalc=function(m,dom) {
 		alias:"",
 		secret:"",
 		len:16,
-		password:""
+		password:"",
 	};
 
-	m.data.bind("set",["alias","secret","len"],function(k,v) {
-		m.data.password=calcPassword(m.data.alias,m.data.secret);
+	m.data.bind("set",[["alias"],["secret"],["len"]],function(k,v) {
+		m.data.password=calcPassword(m.data);
 	});
 
-	var calcPassword = function(alias,secret) {
-		if (secret===""||alias==="") return "";
-		var array = Sha1.hash(secret + alias).match(/.{1,2}/g);
+	var calcPassword = function(d) {
+		if (d.secret===""||d.alias==="") return "";
+		var array = Sha1.hash(d.secret + d.alias).match(/.{1,2}/g);
 		for (var i in array) array[i] = parseInt(array[i], 16);
-		return btoa(String.fromCharCode.apply(null, array)).substring(0,m.data.len);
+		return btoa(String.fromCharCode.apply(null, array)).substring(0,d.len);
 	};
 
 	m.ui={
+	    parts:"children",
 		children: {
-			header: {type:"label", label:"Password Calculator"},
-			len: {type:"select", choices:{"8":"8 chars","16":"16 chars","32":"32 chars"}},
-			alias:  {type:"input", placeholder:"alias", class:"first"},
-			secret: {type:"input", placeholder:"secret", kind:"password"},
-			password: "text"
+			header: {parts:"label", label:"Password Calculator"},
+			len: {parts:"select", choices:{"8":"8 chars","16":"16 chars","32":"32 chars"}},
+			alias:  {parts:"input", placeholder:"alias", class:"first"},
+			secret: {parts:"input", placeholder:"secret", kind:"password"},
+			password: "text",
 		},
 		class:"pwcalc mui"
 	};
 
 };
-
-var main=pwcalc;
